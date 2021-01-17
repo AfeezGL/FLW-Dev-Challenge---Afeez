7de680ae007ed4b013ef4b45ee6c2bc4cd6f724f
Afeez G. Lawal <afeezlg@gmail.com>
2021-01-17 06:26:55 +0100

    store views cleanup

--- store/urls.py
+++ store/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<slug>/products/', views.product_list, name="product_list"),
+    path('<slug>/products/', views.ProductList, name="product_list"),
     path('<slug>/cart/', views.CartView, name = "cart" ),
-    path('dashboard/', views.dashboard, name="dashboard"),
+    path('dashboard/', views.Dashboard, name="dashboard"),
     path('create/', views.CreateStore.as_view(), name="create_store"),
     path('activate/', views.ActivateStore, name="activate_store"),
     path('orders/pending/', views.PendingOrders, name="pending_orders"),
     path('orders/details/<int:pk>/', views.OrderDetails.as_view(), name="order_details"),
     path('initialize_payment', views.InitializePayment, name="activation_payment_init"),
-    path('verify_payment', views.verify_payment, name="payment_verify"),
-    path('add_product', views.AddProduct.as_view(), name="add_product"),
+    path('verify_payment', views.VerifyPayment, name="payment_verify"),
+    path('add_product', views.CreateProduct.as_view(), name="add_product"),
     path('update/', views.UpdateStore.as_view(), name="update_store")
 ]
--- store/views.py
+++ store/views.py
@@ -11,98 +11,22 @@ from .forms import CreateStoreForm, AddProductForm
 from .mixins import HasStoreMixin
 from .decorators import has_store, has_inactive_store
 from datetime import datetime
-import json
 import requests
 from django.conf import settings
 
 FLUTTERWAVE_SEC_KEY = settings.FLUTTERWAVE_SEC_KEY
 BASE_URL = settings.BASE_URL
 
-def product_list(request, slug):
-    try:
-        store = Store.objects.get(slug=slug)
-    except:
-        return HttpResponseNotFound("page not found")
-
-    template = "product/index.html"
-    product_list = store.product_set.all().order_by("-date_updated")
-    paginator = Paginator(product_list, 9)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        "store": store,
-        "object_list": page_obj
-    }
-    return render(request, template, context)
 
 
-def CartView(request, slug):
-    try:
-        store = Store.objects.get(slug=slug)
-    except:
-        return HttpResponseNotFound("page not found")
-    
-    try:
-        customer = Customer.objects.get(user = request.user)
-    except:
-        try:
-            device_id = request.COOKIES["deviceId"]
-        except:
-            device_id = "empty"
-        customer, created = Customer.objects.get_or_create(device_id = device_id)
-    order, created = Order.objects.get_or_create(customer = customer, store = store, completed = False)
-    cartitems = order.cartitem_set.all()
-    template = "product/cart.html"
-    return render(request, template, {"order":order,
-    "cartitems":cartitems})
+"""
+    STORE ADMIN RELATED VIEWS      STORE ADMIN RELATED VIEWS      STORE ADMIN RELATED VIEWS
+    STORE ADMIN RELATED VIEWS      STORE ADMIN RELATED VIEWS      STORE ADMIN RELATED VIEWS
+    STORE ADMIN RELATED VIEWS      STORE ADMIN RELATED VIEWS      STORE ADMIN RELATED VIEWS
+    STORE ADMIN RELATED VIEWS      STORE ADMIN RELATED VIEWS      STORE ADMIN RELATED VIEWS
+    STORE ADMIN RELATED VIEWS      STORE ADMIN RELATED VIEWS      STORE ADMIN RELATED VIEWS
+"""
 
-
-
-@login_required
-@has_store
-def dashboard(request):
-    store = request.user.store
-    template = "store/index.html"
-    product_list = store.product_set.all().order_by("-date_updated")
-    orders = Order.objects.filter(store=store, completed=True)
-    deliveries = []
-    for order in orders:
-        if order.status != "DE":
-            deliveries.append(order)
-    length = len(deliveries)
-    context = {
-        "store": store,
-        "product_list": product_list,
-        "length": length
-
-    }
-    return render(request, template, context)
-
-
-def PendingOrders(request):
-    store = request.user.store
-    template = "store/pendingOrders.html"
-    orders = Order.objects.filter(store=store, completed=True)
-    pending_orders = []
-    for order in orders:
-        if order.status != "DE":
-            pending_orders.append(order)
-    context = {
-        "store": store,
-        "pending_orders": pending_orders
-    }
-    return render(request, template, context)
-
-
-class OrderDetails(UpdateView):
-    model = Order
-    template_name = "store/orderDetails.html"
-    fields = ["status"]
-
-    def get_success_url(self):
-        return reverse("order_details", kwargs={"pk": self.object.id})
-
-  
 class CreateStore(LoginRequiredMixin, CreateView):
     form_class = CreateStoreForm
     template_name = "product/forms.html"
@@ -112,6 +36,7 @@ class CreateStore(LoginRequiredMixin, CreateView):
         store.user = self.request.user
         store.save()
         return HttpResponseRedirect(reverse("dashboard"))
+
 
 @login_required
 @has_inactive_store
@@ -127,6 +52,7 @@ def ActivateStore(request):
         "store": store
     }
     return render(request, template, context)
+
 
 @login_required
 @has_inactive_store
@@ -163,7 +89,7 @@ def InitializePayment(request):
     return HttpResponseRedirect(link)
 
 
-def verify_payment(request):
+def VerifyPayment(request):
     transaction_id = request.GET['transaction_id']
     url = f"https://api.flutterwave.com/v3/transactions/{transaction_id}/verify"
     res = requests.get(url, headers = {"Authorization": FLUTTERWAVE_SEC_KEY})
@@ -185,6 +111,62 @@ def verify_payment(request):
         return JsonResponse(message, safe=False)
 
 
+@login_required
+@has_store
+def Dashboard(request):
+    store = request.user.store
+    template = "store/index.html"
+    product_list = store.product_set.all().order_by("-date_updated")
+    orders = Order.objects.filter(store=store, completed=True)
+    deliveries = []
+    for order in orders:
+        if order.status != "DE":
+            deliveries.append(order)
+    length = len(deliveries)
+    context = {
+        "store": store,
+        "product_list": product_list,
+        "length": length
+
+    }
+    return render(request, template, context)
+
+
+class CreateProduct(LoginRequiredMixin, HasStoreMixin, CreateView):
+    form_class = AddProductForm
+    template_name = "product/forms.html"
+
+    def form_valid(self, form):
+        product = form.save(commit=False)
+        product.store_id = self.request.user.store.id
+        product.save()
+        return HttpResponseRedirect(reverse("add_product"))
+
+
+def PendingOrders(request):
+    store = request.user.store
+    template = "store/pendingOrders.html"
+    orders = Order.objects.filter(store=store, completed=True)
+    pending_orders = []
+    for order in orders:
+        if order.status != "DE":
+            pending_orders.append(order)
+    context = {
+        "store": store,
+        "pending_orders": pending_orders
+    }
+    return render(request, template, context)
+
+
+class OrderDetails(UpdateView):
+    model = Order
+    template_name = "store/orderDetails.html"
+    fields = ["status"]
+
+    def get_success_url(self):
+        return reverse("order_details", kwargs={"pk": self.object.id})
+
+
 class UpdateStore(LoginRequiredMixin, HasStoreMixin, UpdateView):
 
     def get_object(self):
@@ -197,12 +179,52 @@ class UpdateStore(LoginRequiredMixin, HasStoreMixin, UpdateView):
     def get_success_url(self):
         return reverse("store")
 
-class AddProduct(LoginRequiredMixin, HasStoreMixin, CreateView):
-    form_class = AddProductForm
-    template_name = "product/forms.html"
 
-    def form_valid(self, form):
-        product = form.save(commit=False)
-        product.store_id = self.request.user.store.id
-        product.save()
-        return HttpResponseRedirect(reverse("add_product"))
+
+"""
+    CUSTOMER RELATED VIEWS      CUSTOMER RELATED VIEWS      CUSTOMER RELATED VIEWS
+    CUSTOMER RELATED VIEWS      CUSTOMER RELATED VIEWS      CUSTOMER RELATED VIEWS
+    CUSTOMER RELATED VIEWS      CUSTOMER RELATED VIEWS      CUSTOMER RELATED VIEWS
+    CUSTOMER RELATED VIEWS      CUSTOMER RELATED VIEWS      CUSTOMER RELATED VIEWS
+    CUSTOMER RELATED VIEWS      CUSTOMER RELATED VIEWS      CUSTOMER RELATED VIEWS
+"""
+
+
+
+def ProductList(request, slug):
+    try:
+        store = Store.objects.get(slug=slug)
+    except:
+        return HttpResponseNotFound("page not found")
+
+    template = "product/index.html"
+    product_list = store.product_set.all().order_by("-date_updated")
+    paginator = Paginator(product_list, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        "store": store,
+        "object_list": page_obj
+    }
+    return render(request, template, context)
+
+
+def CartView(request, slug):
+    try:
+        store = Store.objects.get(slug=slug)
+    except:
+        return HttpResponseNotFound("page not found")
+    
+    try:
+        customer = Customer.objects.get(user = request.user)
+    except:
+        try:
+            device_id = request.COOKIES["deviceId"]
+        except:
+            device_id = "empty"
+        customer, created = Customer.objects.get_or_create(device_id = device_id)
+    order, created = Order.objects.get_or_create(customer = customer, store = store, completed = False)
+    cartitems = order.cartitem_set.all()
+    template = "product/cart.html"
+    return render(request, template, {"order":order,
+    "cartitems":cartitems})
