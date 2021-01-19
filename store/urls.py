6aa1271125edfae95b51c15d0bf6e2d52bc8da3a
Afeez G. Lawal <afeezlg@gmail.com>
2021-01-19 22:28:33 +0100

    bettr withdrawals

--- store/urls.py
+++ store/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<slug>/products/', views.ProductList, name="product_list"),
+    path('<slug>/', views.ProductList, name="product_list"),
     path('<slug>/cart/', views.CartView, name = "cart" ),
     path('', views.Dashboard, name="dashboard"),
-    path('create/', views.CreateStore.as_view(), name="create_store"),
-    path('activate/', views.ActivateStore, name="activate_store"),
+    path('create/your/store/', views.CreateStore.as_view(), name="create_store"),
+    path('activate/yout/store/', views.ActivateStore, name="activate_store"),
     path('orders/pending/', views.PendingOrders, name="pending_orders"),
     path('orders/details/<int:pk>/', views.OrderDetails.as_view(), name="order_details"),
     path('initialize_payment', views.InitializePayment, name="activation_payment_init"),
